e3549e04391dc2e4c983c12215b246a18d4e7344
UVER <73294472+UVE-R@users.noreply.github.com>
2021-07-12 01:20:34 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+
+dp = [0] * 201  #number of ways to get sum = i
+dp[0] = 1 #1 way of getting sum = 0
+coins = [1,2,5,10,20,50,100,200] #coin list
+
+for coin in coins:
+    for i in range(0,201):
+        if i-coin >=0: #stop index error
+            dp[i] += dp[i-coin] #add new ways
+
+print(dp[200])
